5614d09da92e2246e2e9cd981b04e232438e8e0d
ABHISHEK-KUMAR-SAW <43957385+ABHISHEK-KUMAR-SAW@users.noreply.github.com>
2020-03-28 21:16:01 +0530

    Update Os Project.cpp

--- Os Project.cpp
+++ Os Project.cpp
@@ -7,10 +7,21 @@ int main()
 	cout<<setfill('*');
 	cout<<"\n\n\t"<<setw(65)<<" Welcome to Chef's Kitchen ";
 	cout<<setw(40)<<'*'<<"\n\n";
-	cout<<"\tEnter Number of Chefs :";
+	cout<<"\t Enter Number of Chefs :";
 	cin>>n;
 	cout<<"\n\n\t Enter total number of ingredients a chef can claim :";
 	cin>>r;
-	resources=(n*r)-(n-1);
-	cout<<"\n\n\t Total number of ingredients required >= "<<resources; 
+	if(n>0 && r>0)
+	{
+		resources=(n*r)-(n-1);
+        cout<<"\n\n\t Total number of ingredients required >= "<<resources;
+    	}
+    	else if(n<=0)
+    	{
+    		cout<<"\n\n\t Resources must be greater than 0"; 
+   	}
+    	else
+    	{
+    		cout<<"\n\n\t Deadlock will not occur even if we have no resource."; 
+    	}
 }
