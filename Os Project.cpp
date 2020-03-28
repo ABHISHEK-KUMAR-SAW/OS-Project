5f9f3b5ca99093be2d453c9dbaabe029240208ed
ABHISHEK-KUMAR-SAW <43957385+ABHISHEK-KUMAR-SAW@users.noreply.github.com>
2020-03-28 13:56:08 +0530

    Add files via upload

---
+++ Os Project.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+#include<iomanip>
+using namespace std;
+int main()
+{
+	int n,r,resources;
+	cout<<setfill('*');
+	cout<<"\n\n\t"<<setw(65)<<" Welcome to Chef's Kitchen ";
+	cout<<setw(40)<<'*'<<"\n\n";
+	cout<<"\tEnter Number of Chefs :";
+	cin>>n;
+	cout<<"\n\n\tEnter total number of ingredients a chef can claim :";
+	cin>>r;
+	resources=(n*r)-(n-1);
+	cout<<"\n\n\tTotal number of ingredients required >= "<<resources; 
+}
